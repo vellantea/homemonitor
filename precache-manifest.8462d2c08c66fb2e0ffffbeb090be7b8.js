e22145e7bcbc8777ff7502b68bf261b4c89e1aab
Angela Vellante <angelavellante@Angelas-MacBook-Pro.local>
2020-09-09 17:19:22 -0400

    Updates

---
+++ precache-manifest.8462d2c08c66fb2e0ffffbeb090be7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc6c7ec163423cc20d03c596dad8808",
+    "url": "/homemonitor/index.html"
+  },
+  {
+    "revision": "309e735c6c11eb812b52",
+    "url": "/homemonitor/static/css/main.4dfbe333.chunk.css"
+  },
+  {
+    "revision": "53e71ec9058cab631c19",
+    "url": "/homemonitor/static/js/2.29c6f8a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/homemonitor/static/js/2.29c6f8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309e735c6c11eb812b52",
+    "url": "/homemonitor/static/js/main.53e852da.chunk.js"
+  },
+  {
+    "revision": "a5cb2deb50ea12a26b53",
+    "url": "/homemonitor/static/js/runtime-main.de16f811.js"
+  },
+  {
+    "revision": "8b4fd5f165d4f03bcbf7e00256f1f5c2",
+    "url": "/homemonitor/static/media/icon.8b4fd5f1.png"
+  }
+]);
